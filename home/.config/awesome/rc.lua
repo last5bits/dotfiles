3750601998e477bceb9c61949fc805aa7f7d142c
Alexey Zhikhartsev <last5bits@gmail.com>
2017-01-24 20:57:53 -0500

    AwesomeWM: Fix bug in the menu

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -150,7 +150,7 @@ end
 -- Create a launcher widget and a main menu
 myawesomemenu = {
    { "hotkeys", function() return false, hotkeys_popup.show_help end},
-   { "manual", terminal .. " -e man awesome" },
+   { "manual", terminal .. " -e \"man awesome\"" },
    { "edit config", terminal .. " -e \"" .. editor .. " " .. awesome.conffile .. "\""},
    { "restart", awesome.restart },
    { "quit", function() awesome.quit() end}
