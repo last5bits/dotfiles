ecca44b35a74c49cae8a27bc0d7b06f46c90bcf3
Alexey Zhikhartsev <last5bits@gmail.com>
2017-01-24 19:59:24 -0500

    AwesomeWM: add a spacer between widgets

---
+++ home/.config/awesome/spacer.lua
@@ -0,0 +1,20 @@
+local setmetatable = setmetatable
+local wibox = require("wibox")
+
+local module={}
+
+local data = {}
+
+local function update()
+
+end
+
+local function new(args)
+  local spacer  = wibox.widget.textbox()
+  spacer:set_text(args.text or "")
+  spacer.width = args.width or 0
+  return spacer
+end
+
+
+return setmetatable(module, { __call = function(_, ...) return new(...) end })
