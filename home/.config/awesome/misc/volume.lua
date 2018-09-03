9e667550577f23abc841a343495bb6908a33e148
Alexey Zhikhartsev <last5bits@gmail.com>
2018-09-03 14:53:42 -0400

    AwesomeWM: add a function for switching default pulseaudio sink

--- home/.config/awesome/misc/volume.lua
+++ home/.config/awesome/misc/volume.lua
@@ -30,4 +30,34 @@ function volume.mic_toggle_mute()
     awful.spawn("pamixer --default-source --toggle-mute")
 end
 
+function volume.switch_default_sink()
+   local pamixer = io.popen("pamixer --list-sinks")
+
+   -- Compile a sink menu
+   local menu = {}
+   for line in pamixer:lines() do
+      -- [alex@lenovski ~]$ pamixer --list-sinks
+      -- Sinks:
+      -- 0 "alsa_output.pci-0000_00_1b.0.analog-stereo" "Built-in Audio Analog Stereo"
+      -- 1 "alsa_output.pci-0000_00_03.0.hdmi-stereo" "Built-in Audio Digital Stereo (HDMI)"
+      if string.match(line, "^[%d+]") then
+         -- Notification message
+         local sink_name = string.match(line, "^[%d+] \".+\" \"(.+)\"")
+         local msg = string.format("Default sink: %s", sink_name)
+
+         -- Callback command
+         local sink = string.match(line, "^[%d+] \"(.+)\" \".+\"")
+         local cmd = string.format("pacmd set-default-sink %s", sink)
+
+         table.insert(menu, {msg, {function() awful.spawn(cmd) end}})
+      end
+   end
+
+   -- Iterate over the menu
+   local timeout = 2
+   local menu_iterator = require("lain.util.menu_iterator")
+   local card_icon_path = "/usr/share/icons/gnome/48x48/devices/audio-card.png"
+   menu_iterator.iterate(menu, timeout, card_icon_path)
+end
+
 return volume
