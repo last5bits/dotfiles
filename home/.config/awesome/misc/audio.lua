53de3812b840d060bcd28dcde6255e8ce3cf4dc7
Alexey Zhikhartsev <last5bits@gmail.com>
2019-07-19 21:28:16 -0400

    AwesomeWM: remove unused functions from misc/audio.lua

--- home/.config/awesome/misc/audio.lua
+++ home/.config/awesome/misc/audio.lua
@@ -1,8 +1,6 @@
 -- Control system audio via pulseaudio:
 --   * System volume up/down
---   * Current client's volume
---   * Switch the default sink (e.g., speakers <-> HDMI)
---   * Switch the current client's sink
+--   * Current client's volume up/down
 -- Copyright: Alexey Zhikhartsev <last [digit five] bits [at] gmail [dot] com>
 
 local audio = {}
@@ -18,32 +16,6 @@ local function show_volume(output)
     local volume_level = string.match(output, "(%d+)")
     local volume_msg = "Volume is at " .. volume_level .. "%"
     notif_id = naughty.notify({ text = volume_msg, replaces_id = notif_id }).id
-end
-
-local function get_sink_menu(get_cmd, get_msg)
-   local pamixer = io.popen("pamixer --list-sinks")
-
-   -- Compile a sink menu
-   local menu = {}
-   for line in pamixer:lines() do
-      -- [alex@lenovski ~]$ pamixer --list-sinks
-      -- Sinks:
-      -- 0 "alsa_output.pci-0000_00_1b.0.analog-stereo" "Built-in Audio Analog Stereo"
-      -- 1 "alsa_output.pci-0000_00_03.0.hdmi-stereo" "Built-in Audio Digital Stereo (HDMI)"
-      if string.match(line, "^[%d+]") then
-         -- Notification message
-         local sink_name = string.match(line, "^[%d]+ \".+\" \"(.+)\"")
-         local msg = get_msg(sink_name)
-
-         -- Callback command
-         local sink = string.match(line, "^[%d]+ \"(.+)\" \".+\"")
-         local cmd = get_cmd(id, sink)
-
-         table.insert(menu, {msg, {function() awful.spawn(cmd) end}})
-      end
-   end
-
-   return menu
 end
 
 -- Courtesy of https://coronalabs.com/blog/2013/04/16/lua-string-magic/
@@ -136,30 +108,6 @@ local function client_down(output)
     end
 end
 
-local function switch_client_sink(output)
-    local c = client.focus
-    if c then
-        id = get_sink_input_id(output, c.pid)
-        if id ~= -1 then
-            -- naughty.notify({ text = string.format("%d!", id) })
-            local pamixer = io.popen("pamixer --list-sinks")
-
-            local menu = get_sink_menu(function (id, sink)
-                  return string.format("pactl move-sink-input %d %s", id, sink)
-               end,
-               function (sink_name)
-                  return string.format("Client's default sink: %s", sink_name)
-               end)
-
-            -- Iterate over the menu
-            local timeout = 2
-            local menu_iterator = require("lain.util.menu_iterator")
-            local card_icon_path = "/usr/share/icons/gnome/48x48/devices/audio-card.png"
-            menu_iterator.iterate(menu, timeout, card_icon_path)
-        end
-    end
-end
-
 --[[ Public functions ]]
 
 function audio.volume_up()
@@ -180,34 +128,12 @@ function audio.toggle_mic_mute()
     awful.spawn("pamixer --default-source --toggle-mute")
 end
 
-function audio.switch_default_sink()
-   local pamixer = io.popen("pamixer --list-sinks")
-
-   -- Compile a sink menu
-   local menu = get_sink_menu(function (_, sink)
-         return string.format("pacmd set-default-sink %s", sink)
-      end,
-      function (sink_name)
-         return string.format("Default sink: %s", sink_name)
-      end)
-
-   -- Iterate over the menu
-   local timeout = 2
-   local menu_iterator = require("lain.util.menu_iterator")
-   local card_icon_path = "/usr/share/icons/gnome/48x48/devices/audio-card.png"
-   menu_iterator.iterate(menu, timeout, card_icon_path)
-end
-
 function audio.client_up()
     awful.spawn.easy_async("pactl list sink-inputs", client_up)
 end
 
 function audio.client_down()
     awful.spawn.easy_async("pactl list sink-inputs", client_down)
-end
-
-function audio.client_switch_sink()
-    awful.spawn.easy_async("pactl list sink-inputs", switch_client_sink)
 end
 
 return audio
