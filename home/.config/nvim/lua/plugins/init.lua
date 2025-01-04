84e750a54e942b152a3b0c0caa304ef5244cfc13
Alexey Zhikhartsev <last5bits@gmail.com>
2025-01-04 16:04:13 -0500

    Add mappings for next/prev diff hunk to NeoVim

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -55,7 +55,28 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require('gitsigns').setup()
+      require('gitsigns').setup{
+        on_attach = function(bufnr)
+          local gitsigns = require('gitsigns')
+
+          -- Navigation
+          vim.keymap.set('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({']c', bang = true})
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          vim.keymap.set('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({'[c', bang = true})
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+        end
+      }
     end,
   },
   {
