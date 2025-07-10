4a579c744fce530c32cd11f29ee33a36da01d886
Alexey Zhikhartsev <last5bits@gmail.com>
2025-07-10 12:07:28 -0400

    [nvim] Check for plugin updates once a day

--- home/.config/nvim/lua/config/lazy.lua
+++ home/.config/nvim/lua/config/lazy.lua
@@ -25,5 +25,9 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "slate" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = true,
+    frequency = 86400, -- check for updates once a day
+  },
 })
