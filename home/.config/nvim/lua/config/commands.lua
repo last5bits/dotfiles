2997c41ca5bf0c07fdfba0f76e8adda7aefafc65
Alexey Zhikhartsev <last5bits@gmail.com>
2026-04-26 19:04:35 -0400

    [nvim] Guard :W command against empty arguments to prevent invalid windo call

--- home/.config/nvim/lua/config/commands.lua
+++ home/.config/nvim/lua/config/commands.lua
@@ -1,6 +1,9 @@
 vim.api.nvim_create_user_command(
   'W',
   function(opts)
+    if opts.args == '' then
+      return
+    end
     local cur_win = vim.api.nvim_get_current_win()
     vim.cmd('windo ' .. opts.args)
     vim.api.nvim_set_current_win(cur_win)
