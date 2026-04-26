d8780ab0b8e61608c0a3287492084afc66f687b2
Alexey Zhikhartsev <last5bits@gmail.com>
2026-04-26 19:03:55 -0400

    [nvim] Move LSP keymaps into LspAttach autocmd for correct buffer scoping

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -47,15 +47,21 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       vim.lsp.enable('clangd')
-      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', '<leader>g', ':ClangdSwitchSourceHeader<CR>', { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'cx', vim.lsp.buf.code_action, { noremap = true, buffer = true, silent = true })
-      vim.keymap.set('n', 'cr', vim.lsp.buf.rename, { noremap = true, buffer = true, silent = true })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local opts = { buffer = args.buf, silent = true }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<leader>g', ':ClangdSwitchSourceHeader<CR>', opts)
+          vim.keymap.set('n', 'cx', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'cr', vim.lsp.buf.rename, opts)
+        end,
+      })
     end,
   },
   {
