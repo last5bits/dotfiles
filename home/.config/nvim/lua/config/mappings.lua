7b3f65bd7b6a4b8157307317edd847f05b5cd4c5
Alexey Zhikhartsev <last5bits@gmail.com>
2026-04-26 19:02:45 -0400

    [nvim] Fix typo in <leader>o mapping that caused E488 trailing characters

--- home/.config/nvim/lua/config/mappings.lua
+++ home/.config/nvim/lua/config/mappings.lua
@@ -2,4 +2,4 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>q", ":bp <BAR> bwipeout #<CR>", { silent = true })
-vim.keymap.set("n", "<leader>o", ":W diffoff<CR>::setl nocursorbind<CR>:only<CR>", { silent = true })
+vim.keymap.set("n", "<leader>o", ":W diffoff<CR>:setl nocursorbind<CR>:only<CR>", { silent = true })
