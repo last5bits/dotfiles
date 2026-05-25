0833ee251da96b2cb1b9e0c27107c2b5013cefc0
Alexey Zhikhartsev <last5bits@gmail.com>
2026-05-25 12:50:03 -0400

    [nvim] Replace Vimscript rooter autocmd with pure Lua

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -58,11 +58,22 @@ vim.api.nvim_create_autocmd('FileType', {
 })
 
 -- Poor man's vim-rooter (git only) using fugitive
-vim.cmd([[augroup misc
-  autocmd BufLeave * let b:last_cwd = getcwd()
-  autocmd BufEnter * if exists('b:last_cwd')
-                  \|   execute 'lcd' b:last_cwd
-                  \| else
-                  \|   silent! Glcd
-                  \| endif
-augroup END]])
+-- last_cwd stores the per-buffer working directory so it can be restored when
+-- switching back to that buffer, rather than always resetting to git root.
+local rooter_group = vim.api.nvim_create_augroup("vim_rooter", { clear = true })
+vim.api.nvim_create_autocmd("BufLeave", {
+  group = rooter_group,
+  callback = function()
+    vim.b.last_cwd = vim.fn.getcwd()
+  end,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = rooter_group,
+  callback = function()
+    if vim.b.last_cwd then
+      vim.cmd("lcd " .. vim.b.last_cwd)
+    else
+      pcall(vim.cmd, "Glcd")
+    end
+  end,
+})
