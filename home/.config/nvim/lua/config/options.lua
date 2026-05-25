27e9538c80167d28e9dbd609f007fb70d3ceeea4
Alexey Zhikhartsev <last5bits@gmail.com>
2026-05-24 21:51:21 -0400

    [nvim] remove smartindent in favour of treesitter indent

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -4,7 +4,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.smartindent = true
 vim.opt.mousehide = true
 vim.opt.mouse = 'a'
 vim.opt.swapfile = false
