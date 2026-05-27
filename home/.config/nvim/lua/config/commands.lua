1912d1e8aabd127b473dcb2afd93e151b9113dc8
Alexey Zhikhartsev <last5bits@gmail.com>
2026-05-27 09:13:34 -0400

    [nvim] Add :L alias for :Lazy

--- home/.config/nvim/lua/config/commands.lua
+++ home/.config/nvim/lua/config/commands.lua
@@ -6,3 +6,5 @@ vim.api.nvim_create_user_command('W', function(opts)
   vim.cmd('windo ' .. opts.args)
   vim.api.nvim_set_current_win(cur_win)
 end, { nargs = '?', desc = 'Just like windo, but restore the current window when done.' })
+
+vim.api.nvim_create_user_command('L', 'Lazy', { desc = 'Alias for :Lazy' })
