41f86a463e3f9f9a10525b3e8cf0c4a479563ba4
Alexey Zhikhartsev <last5bits@gmail.com>
2026-06-01 16:51:41 -0400

    [nvim] Factor out on_attach for clangd and pyright LSP

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -45,22 +45,28 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
-      vim.lsp.enable('clangd')
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr, silent = true }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'cx', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'cr', vim.lsp.buf.rename, opts)
+      end
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local opts = { buffer = args.buf, silent = true }
-          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'gy', vim.lsp.buf.type_definition, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', '<leader>g', ':ClangdSwitchSourceHeader<CR>', opts)
-          vim.keymap.set('n', 'cx', vim.lsp.buf.code_action, opts)
-          vim.keymap.set('n', 'cr', vim.lsp.buf.rename, opts)
-        end,
+      vim.lsp.config('pyright', {
+        on_attach = on_attach,
+        root_markers = { 'pyrightconfig.json' },
       })
+      vim.lsp.enable('pyright')
+
+      vim.lsp.config('clangd', {
+        on_attach = on_attach,
+      })
+      vim.lsp.enable('clangd')
     end,
   },
   {
